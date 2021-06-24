95c80435ed80bb689f971955fc4d93f904508c2a
朝宏 张 <zch738416932@hotmail.com>
2021-06-24 11:09:14 +0800

    重新建立WpfApp1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,12 +13,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            
+            
 
-            string str = "Data Source=127.0.0.1;User ID = root;Password = admin;Database=shengangdb;charset = utf8;pooling = true";
-            MySqlConnection _connection = new MySqlConnection();
-            _connection.ConnectionString = str;
-            _connection.Open();
-            Console.WriteLine(str);
             Console.ReadLine();
         }
     }
