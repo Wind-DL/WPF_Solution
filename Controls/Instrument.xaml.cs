abd0834530368c686841e4a2c3fa29a7b58768c9
朝宏 张 <zch738416932@hotmail.com>
2021-06-25 17:11:31 +0800

    自定义控件

--- Controls/Instrument.xaml.cs
+++ Controls/Instrument.xaml.cs
@@ -20,9 +20,56 @@ namespace Controls
     /// </summary>
     public partial class Instrument : UserControl
     {
+
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Value.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(Instrument), new PropertyMetadata(double.NaN,
+                new PropertyChangedCallback(OnPropertyChanged)
+                ));
+
+        public static void OnPropertyChanged(DependencyObject d,DependencyPropertyChangedEventArgs e)
+        {
+            (d as Instrument).Refresh();
+        }
+
         public Instrument()
         {
             InitializeComponent();
+            SizeChanged += Instrument_SizeChanged;
+        }
+
+        private void Instrument_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double minSize = Math.Min(RenderSize.Width, RenderSize.Height);
+            backEllispse.Height = minSize;
+            backEllispse.Width = minSize;
+        }
+
+         void Refresh()
+        {
+            double radius = backEllispse.Width / 2;
+            mainCanvas.Children.Clear();
+            double min = 0;
+            double max = 100;
+            double step = 270.0 / (max - min);
+            for (int i = 0; i < max - min; i++)
+            {
+                Line lineScale = new Line();
+                lineScale.X1 = 100;
+                lineScale.Y1 = 100;
+                lineScale.X2 = 200;
+                lineScale.Y2 = 300;
+                lineScale.Stroke = Brushes.White;
+                lineScale.StrokeThickness = 2;
+                mainCanvas.Children.Add(lineScale);
+            }
         }
     }
 }
