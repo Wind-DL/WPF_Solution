da5e8820e2b90c9d10799668eedbff6546946366
朝宏 张 <zch738416932@hotmail.com>
2021-07-01 12:50:46 +0800

    缩放自适应，PathListBox

--- WpfTest/MyControls/UintQuery.xaml.cs
+++ WpfTest/MyControls/UintQuery.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,61 @@ namespace WpfTest.MyControls
     /// </summary>
     public partial class UintQuery : UserControl
     {
+
+        public List<string> Times
+        {
+            get { return (List<string>)GetValue(TimesProperty); }
+            set { SetValue(TimesProperty, value); }
+        }
+        public static readonly DependencyProperty TimesProperty =
+            DependencyProperty.Register("Times", typeof(List<string>), typeof(UintQuery), new PropertyMetadata(null,new PropertyChangedCallback(CallBack)));
+
+
+        public int MyProperty
+        {
+            get { return (int)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("MyProperty", typeof(int), typeof(UintQuery), new PropertyMetadata(0, new PropertyChangedCallback(CallBack)));
+
+        static void CallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+
+        static void Refresh()
+        {
+            //Console.WriteLine("hello");
+        }
+
+       public List<Times> TimeValues { set; get; } = new List<Times>();
         public UintQuery()
         {
             InitializeComponent();
+            TimeValues.Add(new Times { Value = "08:30" });
+            TimeValues.Add(new Times { Value = "09:30" });
+            TimeValues.Add(new Times { Value = "10:30" });
+            TimeValues.Add(new Times { Value = "11:30" });
+            TimeValues.Add(new Times { Value = "12:30" });
+            TimeValues.Add(new Times { Value = "13:30" });
+            TimeValues.Add(new Times { Value = "14:30" });
+            TimeValues.Add(new Times { Value = "15:30" });
+            TimeValues.Add(new Times { Value = "16:30" });
+            TimeValues.Add(new Times { Value = "17:30" });
+            TimeValues.Add(new Times { Value = "18:30" });
+            TimeValues.Add(new Times { Value = "19:30" });
+            TimeValues.Add(new Times { Value = "20:30" });
+
+            DataContext = this;
         }
+    }
+
+    public class Times
+    {
+        public string Value { set; get; }
     }
 }
